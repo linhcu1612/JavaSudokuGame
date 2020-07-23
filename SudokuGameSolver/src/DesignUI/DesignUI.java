27d4881279412e99945c45d052cb037015a4c5cc
NgocNhatLinhCu <ngocnhatlinh.cu@students.mq.edu.au>
2020-07-23 10:48:31 +1000

    UI

--- SudokuGameSolver/src/DesignUI/DesignUI.java
+++ SudokuGameSolver/src/DesignUI/DesignUI.java
@@ -1,17 +1,26 @@
 package DesignUI;
 
-import java.awt.GridLayout;
- 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 public class DesignUI {
+    JFrame f;
 
-    
-    public static void main(String[] args) {
+    public DesignUI() {
+        f = new JFrame();
 
+        JButton gameMode1 = new JButton();
+        gameMode1.setText("CreatingMode");
+        gameMode1.setBounds(100, 225, 100, 50);
+        JButton gameMode2 = new JButton();
+        gameMode2.setText("SolvingMode");
+        gameMode2.setBounds(300, 225, 100, 50);
+        f.add(gameMode1);
+        f.add(gameMode2);
+        f.setSize(500, 500);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
-    
-    
 }
--- SudokuGameSolver/src/GamePlay/Client.java
+++ SudokuGameSolver/src/GamePlay/Client.java
@@ -1,13 +1,13 @@
 package GamePlay;
 
+import DesignUI.DesignUI;
+
 public class Client {
     public static void main(String[] args) {
+        
         Game game = new Game();
         int[][] sudoku = game.randSudoku();
-        game.solvedSudoku(sudoku);
-        game.generateSudoku(sudoku);
-        game.printSudoku(sudoku);
+        new DesignUI();
         
-    
     }
 }
