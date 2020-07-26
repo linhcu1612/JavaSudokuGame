64c9c56acd8d1e4b529eda63c21dc50c21fb4b78
NgocNhatLinhCu <ngocnhatlinh.cu@students.mq.edu.au>
2020-07-26 20:56:10 +1000

    ...

---
+++ SudokuGameSolver/src/DesignUI/DrawPanel.java
@@ -0,0 +1,54 @@
+package DesignUI;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+
+class DrawPanel extends JPanel {
+    private void drawSudoku(Graphics g) {
+
+        var g2d = (Graphics2D) g;
+    
+        int boxHeight = this.getHeight() / 9;
+        int boxWidth = this.getWidth() / 9;
+
+        int sudokuWidth = (this.getWidth() / 9) * 9;
+        int sudokuHeight = (this.getHeight() / 9) * 9;
+
+        g2d.setColor(Color.white);
+    
+        g2d.fillRect(0, 0,this.getWidth(),this.getHeight());
+        
+        g2d.setColor(Color.BLACK);
+        for(int x = 0;x <= this.getWidth();x+=boxWidth) {
+            if((x/(this.getWidth()/9)) % 3 == 0) {
+                g2d.setStroke(new BasicStroke(2));
+                g2d.drawLine(x, 0, x, sudokuHeight);
+            }
+            else {
+                g2d.setStroke(new BasicStroke(1));
+                g2d.drawLine(x, 0, x, sudokuHeight);
+            }
+        }
+    
+        for(int y = 0;y <= this.getHeight();y+=boxHeight) {
+            if((y/(this.getHeight()/9)) % 3 == 0) {
+                g2d.setStroke(new BasicStroke(2));
+                g2d.drawLine(0, y, sudokuWidth, y);
+            }
+            else {
+                g2d.setStroke(new BasicStroke(1));
+                g2d.drawLine(0, y, sudokuWidth, y);
+            }
+        }
+    }
+
+    @Override
+    public void paintComponents(Graphics g) {
+
+        super.paintComponents(g);
+        drawSudoku(g);
+    }
+}
---
+++ SudokuGameSolver/src/DesignUI/PointsEx.java
@@ -0,0 +1,34 @@
+package DesignUI;
+
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+
+public class PointsEx extends JFrame {
+
+    public PointsEx() {
+        
+        initUI();
+    }
+
+    private void initUI() {
+        
+        var drawPanel = new DrawPanel();
+        add(drawPanel);
+
+
+        setSize(450, 450);
+        setTitle("Points");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            
+            var ex = new PointsEx();
+            ex.setVisible(true);
+        });
+    }
+}
+    
