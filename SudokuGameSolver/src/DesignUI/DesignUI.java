c0461c3defb9c473951251d50ab443ea0180cba2
NgocNhatLinhCu <ngocnhatlinh.cu@students.mq.edu.au>
2020-07-26 16:49:11 +1000

    modified file

--- SudokuGameSolver/src/DesignUI/DesignUI.java
+++ SudokuGameSolver/src/DesignUI/DesignUI.java
@@ -3,8 +3,15 @@ package DesignUI;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class DesignUI {
+public class DesignUI extends JFrame {
     JFrame f;
 
     public DesignUI() {
@@ -13,18 +20,97 @@ public class DesignUI {
         JButton gameMode1 = new JButton();
         gameMode1.setText("CreatingMode");
         gameMode1.setBounds(100, 225, 100, 50);
-        gameMode1.addMouseListener(l);
+        gameMode1.addMouseListener(new CustomMouseListener());
         JButton gameMode2 = new JButton();
         gameMode2.setText("SolvingMode");
         gameMode2.setBounds(300, 225, 100, 50);
+        //gameMode2.addMouseListener(new CustomMouseListener());
         
-        
-
+        f.repaint();
         f.add(gameMode1);
         f.add(gameMode2);
         f.setSize(500, 500);
         f.setLayout(null);
         f.setVisible(true);
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    class DrawPanel extends JPanel {
+        private void drawSudoku(Graphics g) {
+
+            var g2d = (Graphics2D) g;
+        
+            int boxHeight = this.getHeight() / 9;
+            int boxWidth = this.getWidth() / 9;
+
+            int sudokuWidth = (this.getWidth() / 9) * 9;
+            int sudokuHeight = (this.getHeight() / 9) * 9;
+
+            g2d.setColor(Color.white);
+		
+    		g2d.fillRect(0, 0,this.getWidth(),this.getHeight());
+            
+    		g2d.setColor(Color.BLACK);
+    		for(int x = 0;x <= this.getWidth();x+=boxWidth) {
+    			if((x/(this.getWidth()/9)) % 3 == 0) {
+	    			g2d.setStroke(new BasicStroke(2));
+	    			g2d.drawLine(x, 0, x, sudokuHeight);
+    			}
+	    		else {
+    				g2d.setStroke(new BasicStroke(1));
+	    			g2d.drawLine(x, 0, x, sudokuHeight);
+	    		}
+    		}
+		
+	    	for(int y = 0;y <= this.getHeight();y+=boxHeight) {
+	    		if((y/(this.getHeight()/9)) % 3 == 0) {
+	    			g2d.setStroke(new BasicStroke(2));
+	    			g2d.drawLine(0, y, sudokuWidth, y);
+	    		}
+	    		else {
+		    		g2d.setStroke(new BasicStroke(1));
+		    		g2d.drawLine(0, y, sudokuWidth, y);
+		    	}
+	    	}
+        }
+
+        @Override
+        public void paintComponents(Graphics g) {
+
+            super.paintComponents(g);
+           drawSudoku(g);
+        }
+    }
+
+    public class CustomMouseListener implements MouseListener {
+		@Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+            System.out.println("this is creatingMode");
+                //var drawPanel = new DrawPanel();
+                //add(drawPanel);
+                //f.repaint();
+
+
+                //setSize(500, 500);
+                //setTitle("Sudoku");
+                //setLocationRelativeTo(null);
+                //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}
     }
 }
